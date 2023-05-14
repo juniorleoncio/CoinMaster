5344b58a354faf67a9ef6dd97df5a9e093139963
Graciano Leoncio <graciano.leoncio@gmail.com>
2023-05-14 20:11:58 -0300

    commit inicial

---
+++ Coin Master/Coin_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coin_MasterApp.swift
+//  Coin Master
+//
+//  Created by junior leoncio on 14/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Coin_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
